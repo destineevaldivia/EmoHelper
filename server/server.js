bea925c23cb3d110b620ad1ed44216fd9ab6acfb
destineevaldivia <destvaldivia@gmail.com>
2023-10-27 10:12:58 -0700

    created databse and added db dump file

--- server/server.js
+++ server/server.js
@@ -4,15 +4,15 @@ import cookieParser from "cookie-parser";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import "dotenv/config";
-//import db from "./db/db-connection.js";
 import querystring from "querystring";
 import axios from "axios";
+//import db from "./db/db-connection.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const REACT_BUILD_DIR = path.join(__dirname, "..", "client", "build");
 
-//initialize instance of express and assign port
+//Initialize instance of express and assign port
 const app = express();
 const PORT = process.env.PORT || 8080;
 //Middleware
@@ -21,16 +21,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(REACT_BUILD_DIR));
 
-//Creates an endpoint for the route /api
+//Serves static webpage
 app.get("/", (req, res) => {
-  //res.json({ message: 'Hello from My template ExpressJS' });
   res.sendFile(path.join(REACT_BUILD_DIR, "index.html"));
 });
-//Requests User Auth from Spotify
+
+//Defining variables that store sensitive information
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
-
 //Generates a random string containing numbers and letters for security reasons to use in Login handler
 const generateRandomString = (length) => {
   let text = "";
@@ -41,10 +40,9 @@ const generateRandomString = (length) => {
   }
   return text;
 };
-const stateKey = "spotify_auth_state";
 
-//Requests authorization from Spotify for specific scope access
-//Spotify authorizes and gives me an Auth Code
+//Requests User Auth from Spotify and specific scope access
+const stateKey = "spotify_auth_state";
 app.get("/login", (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
@@ -58,7 +56,7 @@ app.get("/login", (req, res) => {
     state: state,
     scope: scope,
   });
-
+  //Receive the auth code and include it in queryParams
   res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 

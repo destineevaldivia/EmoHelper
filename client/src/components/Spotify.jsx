9b73f32757b7f8cc5775d034bbeec826b3d05803
destineevaldivia <destvaldivia@gmail.com>
2023-10-28 11:31:03 -0700

    added parameters to the get req to retrieve 50 tracks and also to randomize the offset so that I'm not always receiving the first 50 from the array of tracks

--- client/src/components/Spotify.jsx
+++ client/src/components/Spotify.jsx
@@ -21,12 +21,21 @@ axios.defaults.baseURL = "https://api.spotify.com/v1";
 axios.defaults.headers["Authorization"] = `Bearer ${accessToken}`;
 axios.defaults.headers["Content-Type"] = "application/json";
 
+//Get req to Spotify endpoint (Get Tracks' Audio Features)
+//https://developer.spotify.com/documentation/web-api/reference/get-several-audio-features
+
+//export const getTracksAudioFeatures = () => axios.get("/audio-features");
+
 /*Get req to Spotify endpoint (Get User's Saved Tracks)
-https://developer.spotify.com/documentation/web-api/reference/get-users-saved-tracks
+https://developer.spotify.com/documentation/web-api/reference/get-users-saved-tracks */
+export const getUsersSavedTracks = () => {
+  // Generate a random offset between 0 and 500 so that I'm not always grabbing the first 50 from my saved music
+  const randomOffset = Math.floor(Math.random() * 500);
 
-Get req to Spotify endpoint (Get Tracks' Audio Features)
-https://developer.spotify.com/documentation/web-api/reference/get-several-audio-features
-*/
-// export const getTracksAudioFeatures = () => axios.get("/audio-features");
-
-export const getUsersSavedTracks = () => axios.get("/me/tracks");
+  return axios.get("/me/tracks", {
+    params: {
+      limit: 50,
+      offset: randomOffset,
+    },
+  });
+};

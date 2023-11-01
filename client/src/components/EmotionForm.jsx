13f56f05ac445c32c4eafd987e260961193922ad
destineevaldivia <destvaldivia@gmail.com>
2023-11-01 14:45:13 -0700

    render form with radio buttons for the array of emotions, allow user to select one emotion

--- client/src/components/EmotionForm.jsx
+++ client/src/components/EmotionForm.jsx
@@ -26,9 +26,36 @@ const EmotionForm = () => {
 
   const [selectedEmotion, setSelectedEmotion] = useState("");
 
+  const handleEmotionChange = (event) => {
+    setSelectedEmotion(event.target.value);
+  };
+
+  //I will want to get the valence score of this emotion by querying the db
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //...work in progress
+    console.log("Selected Emotion:", selectedEmotion);
+  };
+
   return (
     <div>
       <h3>Which of these emotions do you want to focus on?</h3>
+      <form onSubmit={handleSubmit}>
+        {emotions.map((emotion) => (
+          <div key={emotion}>
+            <label>
+              <input
+                type="radio"
+                value={emotion}
+                checked={selectedEmotion === emotion}
+                onChange={handleEmotionChange}
+              />
+              {emotion}
+            </label>
+          </div>
+        ))}
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };

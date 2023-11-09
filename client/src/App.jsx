8ae10536fbc34cce7de508a122c9c348d93c3b12
destineevaldivia <destvaldivia@gmail.com>
2023-11-09 13:18:20 -0800

    add comments to app.jsx component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+//import the accessToken and utility functions from SpotifyApi.js file
 import {
   accessToken,
   getUsersSavedTracks,
@@ -7,29 +8,32 @@ import {
 import EmotionForm from "./components/EmotionForm";
 import { ToastContainer } from "react-toastify";
 
-//import the accessToken and utility functions from SpotifyApi.js file
-//use token state to conditionally render login button
 function App() {
+  // state variables
   const [token, setToken] = useState(null);
   const [savedTracks, setSavedTracks] = useState(null);
   const [audioFeatures, setAudioFeatures] = useState(null);
 
+  // useEffect to set the token and fetch user data when the component mounts
   useEffect(() => {
     setToken(accessToken);
 
     const fetchData = async () => {
       try {
+        // Fetch user's saved tracks
         const { data } = await getUsersSavedTracks();
         setSavedTracks(data);
 
+        // Fetch audio features of the saved tracks
         const audioFeaturesData = await getTracksAudioFeatures(data);
         setAudioFeatures(audioFeaturesData);
-      } catch (e) {
-        console.error(e);
+      } catch (error) {
+        console.error("Error fetching at app.jsx:", error);
       }
     };
+    // Call the fetchData function
     fetchData();
-  }, []);
+  }, []); // Empty dependency array means this effect runs only once, on mount
   console.log("savedTracks state successfully updated ", savedTracks);
   console.log("audiofeatures state successfully updated ", audioFeatures);
 
@@ -38,6 +42,7 @@ function App() {
       <div>
         <h1 className="App-title">EmoHelper </h1>
         <h4> *~*~* validation journal *~*~* </h4>
+        {/* Conditionally render login button and emotion form component*/}
         {!token ? (
           <a className="App-link" href="http://localhost:8080/login">
             <button>Log in using Spotify</button>
@@ -54,6 +59,7 @@ function App() {
           </div>
         )}
       </div>
+      {/* Toast notifications container */}
       <ToastContainer />
     </>
   );

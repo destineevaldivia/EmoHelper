dce963e018d4d7941b411d8224dd49f6af33e330
destineevaldivia <destvaldivia@gmail.com>
2023-11-09 13:53:28 -0800

    add comments for emoEntry component

--- client/src/components/EmoEntry.jsx
+++ client/src/components/EmoEntry.jsx
@@ -1,10 +1,14 @@
+// receives formData as a prop
 const EmoEntry = ({ formData }) => {
   //create date function
   console.log("formData accessed:", formData);
   const getCurrentDate = () => {
     return new Date().toLocaleDateString();
   };
+  // get current date
   const currentDate = getCurrentDate();
+
+  // use formData to create an interpolated statement for the emoEntry
   return (
     <div>
       <h3>Here is your Emo Entry:</h3>
